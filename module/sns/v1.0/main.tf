88730a68d06bb1209223fc8cc69ec24a02bd0097
Anonymint <mint.com@gmail.com>
2017-08-24 15:53:23 -0500

    Fix the error syntax

--- module/sns/v1.0/main.tf
+++ module/sns/v1.0/main.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic" "topic" {
   count = "${length(var.name_list)}"
   name = "${var.environment}-${var.region}-${element(var.name_list, count.index)}"
-  display_name = "${element(var.name-list, count.index)}"
+  display_name = "${element(var.name_list, count.index)}"
 }
